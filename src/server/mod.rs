82674e971365e8870f3d2931bed21bc8efc22d2d
yacn <admin@yacn.me>
2015-03-26 20:29:51 -0400

    removes uneeded http module

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -29,16 +29,12 @@ use std::io::{
 use std::thread::Thread;
 use std::io::net::tcp::TcpAcceptor;
 
-use self::http::RequestLine;
-
 use super::find_route;
 use super::subway::{Subway, SubwayGraph, Query};
 use super::subway::Query::{Route, Enable, Disable};
 
 // chosen arbitrarily
 const MAX_QUERY_LENGTH: usize = 1024;
-
-pub mod http;
 
 pub fn start(bind_addr: &str, shared_subway: Arc<Mutex<Subway>>) {
     type Message = (Result<Query, String>, Sender<String>);
@@ -121,32 +117,5 @@ pub fn start(bind_addr: &str, shared_subway: Arc<Mutex<Subway>>) {
                 });
             }
         }
-    }
-}
-
-
-
-pub fn respond<S: io::Stream>(mut stream: &mut S, request_line: &str) -> () {
-    let maybe_request_line: Option<RequestLine> = RequestLine::from_str(request_line);
-
-    if maybe_request_line.is_none() { return http::bad_request(stream) }
-
-    let rline: RequestLine = maybe_request_line.unwrap();
-
-    let cwd = os::getcwd().unwrap();
-    
-    // remove the initial `/' before joining so path doesn't think file is absolute path
-    let path = cwd.join(rline.path.slice_from(1));
-
-    let check_for: [&str; 4] = [path.as_str().unwrap_or(""), "index.html",
-                                "index.shtml", "index.txt"];
-
-    let maybe_path: Option<Path> = check_for.iter()
-                                            .map(|&: s| path.join(*s))
-                                            .filter(|&: p| p.exists() &&
-                                                          !p.is_dir()).next();
-    match maybe_path {
-        Some(p) => { return http::response(stream, &p); },
-        None    => { return http::not_found(stream); },
     }
 }
